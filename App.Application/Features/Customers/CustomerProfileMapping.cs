3455504d89561e25772fceccc49b0259d2fa204a
Enes Pehlivan <pehlivan.enes53@gmail.com>
2024-11-02 13:56:30 +0300

    CustomerProfileMapping and EventProfileMapping added

---
+++ App.Application/Features/Customers/CustomerProfileMapping.cs
@@ -0,0 +1,26 @@
+﻿using App.Application.Features.Customers.Create;
+using App.Application.Features.Customers.Dto;
+using App.Application.Features.Customers.Update;
+using App.Domain.Entities;
+using AutoMapper;
+
+namespace App.Application.Features.Customers
+{
+    public class CustomerProfileMapping : Profile
+    {
+        public CustomerProfileMapping()
+        {
+            CreateMap<CustomerDto, Customer>().ReverseMap();
+
+            CreateMap<CreateCustomerRequest, Customer>()
+                .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.FirstName.ToLowerInvariant()))
+                .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.LastName.ToLowerInvariant()))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email.ToLowerInvariant()));
+
+            CreateMap<UpdateCustomerRequest, Customer>()
+                .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.FirstName.ToLowerInvariant()))
+                .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.LastName.ToLowerInvariant()))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email.ToLowerInvariant()));
+        }
+    }
+}
---
+++ App.Application/Features/Events/EventProfileMapping.cs
@@ -0,0 +1,24 @@
+﻿using App.Application.Features.Events.Create;
+using App.Application.Features.Events.Dto;
+using App.Application.Features.Events.Update;
+using App.Domain.Entities;
+using AutoMapper;
+
+namespace App.Application.Features.Events
+{
+    public class EventProfileMapping : Profile
+    {
+        public EventProfileMapping()
+        {
+            CreateMap<EventDto, Event>().ReverseMap();
+
+            CreateMap<CreateEventRequest, Event>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name.ToLowerInvariant()))
+                .ForMember(dest => dest.Address, opt => opt.MapFrom(src => src.Address.ToLowerInvariant()));
+
+            CreateMap<UpdateEventRequest, Event>()
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name.ToLowerInvariant()))
+                .ForMember(dest => dest.Address, opt => opt.MapFrom(src => src.Address.ToLowerInvariant()));
+        }
+    }
+}
