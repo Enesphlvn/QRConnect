4b13b19616d38b84556d3339de3682481688625a
Enes Pehlivan <pehlivan.enes53@gmail.com>
2024-11-02 14:03:54 +0300

    TicketProfileMapping class added

---
+++ App.Application/Features/Tickets/TicketProfileMapping.cs
@@ -0,0 +1,20 @@
+﻿using App.Application.Features.Tickets.Create;
+using App.Application.Features.Tickets.Dto;
+using App.Application.Features.Tickets.Update;
+using App.Domain.Entities;
+using AutoMapper;
+
+namespace App.Application.Features.Tickets
+{
+    public class TicketProfileMapping : Profile
+    {
+        public TicketProfileMapping()
+        {
+            CreateMap<TicketDto, Ticket>().ReverseMap();
+
+            CreateMap<CreateTicketRequest, Ticket>();
+
+            CreateMap<UpdateTicketRequest, Ticket>();
+        }
+    }
+}
