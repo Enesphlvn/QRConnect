d0221fddcd9d1d8992e680caedb31f7381e16b55
Enes Pehlivan <pehlivan.enes53@gmail.com>
2024-11-02 14:56:06 +0300

    Added ServiceExtensions class

---
+++ App.API/Controllers/CustomersController.cs
@@ -0,0 +1,14 @@
+﻿using App.Application.Features.Customers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.API.Controllers
+{
+    public class CustomersController(ICustomerService customerService) : CustomBaseController
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetCustomers()
+        {
+            return CreateActionResult(await customerService.GetAllListAsync());
+        }
+    }
+}
