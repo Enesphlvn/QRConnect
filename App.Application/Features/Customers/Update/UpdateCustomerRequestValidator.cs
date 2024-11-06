2ec884fd2d469b10e32e69757862a1e46a3ea899
Enes Pehlivan <pehlivan.enes53@gmail.com>
2024-11-06 17:35:48 +0300

    validator class created for UpdateCustomerRequest

---
+++ App.Application/Features/Customers/Update/UpdateCustomerRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace App.Application.Features.Customers.Update
+{
+    public class UpdateCustomerRequestValidator : AbstractValidator<UpdateCustomerRequest>
+    {
+        public UpdateCustomerRequestValidator()
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty().WithMessage("İsim zorunludur.")
+                .Length(3, 50).WithMessage("İsim 3 ile 50 karakter arasında olmalıdır.");
+
+            RuleFor(x => x.LastName)
+                .NotEmpty().WithMessage("Soyisim zorunludur.")
+                .Length(3, 50).WithMessage("Soyisim 3 ile 50 karakter arasında olmalıdır.");
+        }
+    }
+}
