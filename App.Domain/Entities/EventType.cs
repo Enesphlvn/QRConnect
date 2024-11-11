8f9f0439ee988e389eb874ff3e365cb51e15e139
Enes Pehlivan <pehlivan.enes53@gmail.com>
2024-11-12 01:19:31 +0300

    Venue and EventType entities created

---
+++ App.Domain/Entities/EventType.cs
@@ -0,0 +1,10 @@
+﻿using App.Domain.Entities.Common;
+
+namespace App.Domain.Entities
+{
+    public class EventType : BaseEntity<int>
+    {
+        public string Name { get; set; } = default!;
+        public List<Event> Events { get; set; } = [];
+    }
+}
---
+++ App.Domain/Entities/Venue.cs
@@ -0,0 +1,13 @@
+﻿using App.Domain.Entities.Common;
+
+namespace App.Domain.Entities
+{
+    public class Venue : BaseEntity<int>
+    {
+        public string Name { get; set; } = default!;
+        public string City { get; set; } = default!;
+        public string District { get; set; } = default!;
+        public int Capacity { get; set; }
+        public List<Event> Events { get; set; } = [];
+    }
+}
